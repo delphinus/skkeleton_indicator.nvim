5101abef42d9238d407e5c0c7f7da31c10dfdfec
delphinus <me@delphinus.dev>
2022-06-12 16:21:23 +0900

    Fix by stylua

--- lua/skkeleton_indicator/indicator.lua
+++ lua/skkeleton_indicator/indicator.lua
@@ -1,7 +1,7 @@
-local api = require'skkeleton_indicator.util'.api
-local fn = require'skkeleton_indicator.util'.fn
-local Autocmd = require'skkeleton_indicator.autocmd'
-local Modes = require'skkeleton_indicator.modes'
+local api = require("skkeleton_indicator.util").api
+local fn = require("skkeleton_indicator.util").fn
+local Autocmd = require "skkeleton_indicator.autocmd"
+local Modes = require "skkeleton_indicator.modes"
 
 local M = {
   funcs = {},
@@ -19,52 +19,59 @@ function M.new(opts)
     ignore_ft = opts.ignore_ft,
     buf_filter = opts.buf_filter,
     is_skkeleton_loaded = false,
-  }, {__index = M})
-  Autocmd.new(opts.module_name):add{
-    {'InsertEnter', '*', self:method'open'},
-    {'InsertLeave', '*', self:method'close'},
-    {'CursorMovedI', '*', self:method'move'},
-    {'User', 'skkeleton-mode-changed', self:method('update', 'mode-changed')},
-    {'User', 'skkeleton-disable-post', self:method('update', 'disable-post')},
-    {'User', 'skkeleton-enable-post', self:method('update', 'enable-post')},
+  }, { __index = M })
+  Autocmd.new(opts.module_name):add {
+    { "InsertEnter", "*", self:method "open" },
+    { "InsertLeave", "*", self:method "close" },
+    { "CursorMovedI", "*", self:method "move" },
+    { "User", "skkeleton-mode-changed", self:method("update", "mode-changed") },
+    { "User", "skkeleton-disable-post", self:method("update", "disable-post") },
+    { "User", "skkeleton-enable-post", self:method("update", "enable-post") },
   }
   return self
 end
 
 function M:is_disabled()
-  if not self.always_shown and
-    (not self.is_skkeleton_loaded or not fn['skkeleton#is_enabled']()) then
+  if not self.always_shown and (not self.is_skkeleton_loaded or not fn["skkeleton#is_enabled"]()) then
     return true
   end
   local buf = api.get_current_buf()
-  local current_ft = api.buf_get_option(buf, 'filetype')
+  local current_ft = api.buf_get_option(buf, "filetype")
   for _, ft in ipairs(self.ignore_ft) do
-    if current_ft == ft then return true end
+    if current_ft == ft then
+      return true
+    end
   end
   return not self.buf_filter(buf)
 end
 
 function M:method(name, ...)
-  local arg = {...}
-  return function() self[name](self, unpack(arg)) end
+  local arg = { ... }
+  return function()
+    self[name](self, unpack(arg))
+  end
 end
 
 function M:set_text(buf)
   local mode = self.modes:detect()
-  api.buf_set_lines(buf, 0, 0, false, {mode.text})
+  api.buf_set_lines(buf, 0, 0, false, { mode.text })
   api.buf_clear_namespace(buf, self.ns, 0, -1)
   api.buf_add_highlight(buf, self.ns, mode.hl_name, 0, 0, -1)
-  if self.timer then fn.timer_stop(self.timer) end
-  self.timer = fn.timer_start(self.fade_out_ms, self:method'close')
+  if self.timer then
+    fn.timer_stop(self.timer)
+  end
+  self.timer = fn.timer_start(self.fade_out_ms, self:method "close")
 end
 
 function M:open()
-  if self.timer or self:is_disabled() then return end
+  if self.timer or self:is_disabled() then
+    return
+  end
   local buf = api.create_buf(false, true)
   self:set_text(buf)
   local winid = api.open_win(buf, false, {
-    style = 'minimal',
-    relative = 'cursor',
+    style = "minimal",
+    relative = "cursor",
     row = 1,
     col = 1,
     height = 1,
@@ -80,11 +87,13 @@ function M:update(event)
     -- update() will be called in InsertLeave because skkeleton invokes the
     -- skkeleton-mode-changed event. So here it should checks mode() to confirm
     -- it is in the Insert mode.
-    if not self.modes:is_insert() then return end
+    if not self.modes:is_insert() then
+      return
+    end
 
-    if event == 'enable-post' then
+    if event == "enable-post" then
       self.is_skkeleton_loaded = true
-    elseif event == 'disable-post' and not self.always_shown then
+    elseif event == "disable-post" and not self.always_shown then
       self:close(is_fast_event)
       return
     end
@@ -96,6 +105,7 @@ function M:update(event)
       self:open()
     end
   end
+
   if vim.in_fast_event() then
     update(true)
   else
@@ -104,25 +114,31 @@ function M:update(event)
 end
 
 function M:move()
-  if not self.timer then return end
+  if not self.timer then
+    return
+  end
   api.win_set_config(self.winid[1], {
-    relative = 'cursor',
+    relative = "cursor",
     row = 1,
     col = 1,
   })
 end
 
 function M:close(is_fast_event)
-  if not self.timer then return end
+  if not self.timer then
+    return
+  end
   fn.timer_stop(self.timer)
   self.timer = nil
-  if #self.winid == 0 then return end
+  if #self.winid == 0 then
+    return
+  end
 
   local function close(winid)
     local buf = api.win_get_buf(winid)
     api.win_close(winid, true)
     api.buf_clear_namespace(buf, self.ns, 0, -1)
-    api.buf_delete(buf, {force = true})
+    api.buf_delete(buf, { force = true })
   end
 
   for i = #self.winid, 1, -1 do
@@ -130,7 +146,9 @@ function M:close(is_fast_event)
     if vim.in_fast_event() or is_fast_event then
       close(winid)
     else
-      vim.schedule(function() close(winid) end)
+      vim.schedule(function()
+        close(winid)
+      end)
     end
     table.remove(self.winid)
   end
--- lua/skkeleton_indicator/modes.lua
+++ lua/skkeleton_indicator/modes.lua
@@ -1,67 +1,73 @@
-local api = require'skkeleton_indicator.util'.api
-local fn = require'skkeleton_indicator.util'.fn
+local api = require("skkeleton_indicator.util").api
+local fn = require("skkeleton_indicator.util").fn
 
 local M = {}
 
 function M.new(opts)
   local self = setmetatable({
     eiji = {
-      hl_name = opts.module_name..'_eiji',
+      hl_name = opts.module_name .. "_eiji",
       text = opts.eiji_text,
-      hl = {fg = 'cyan', bg = 'black', bold = true},
+      hl = { fg = "cyan", bg = "black", bold = true },
     },
     hira = {
-      hl_name = opts.module_name..'_hira',
-      hl = {fg = 'black', bg = 'green', bold = true},
+      hl_name = opts.module_name .. "_hira",
+      hl = { fg = "black", bg = "green", bold = true },
       text = opts.hira_text,
     },
     kata = {
-      hl_name = opts.module_name..'_kata',
-      hl = {fg = 'black', bg = 'yellow', bold = true},
+      hl_name = opts.module_name .. "_kata",
+      hl = { fg = "black", bg = "yellow", bold = true },
       text = opts.kata_text,
     },
     hankata = {
-      hl_name = opts.module_name..'_hankata',
-      hl = {fg = 'black', bg = 'magenta', bold = true},
+      hl_name = opts.module_name .. "_hankata",
+      hl = { fg = "black", bg = "magenta", bold = true },
       text = opts.hankata_text,
     },
     zenkaku = {
-      hl_name = opts.module_name..'_zenkaku',
-      hl = {fg = 'black', bg = 'cyan', bold = true},
+      hl_name = opts.module_name .. "_zenkaku",
+      hl = { fg = "black", bg = "cyan", bold = true },
       text = opts.zenkaku_text,
     },
     width = 0,
-  }, {__index = M})
+  }, { __index = M })
 
   api._set_hl_ns(opts.ns)
   -- opts.eiji_hl_name
   -- opts.hira_hl_name
   -- opts.kata_hl_name
   -- opts.hankata_hl_name
-  for _, v in ipairs{'eiji', 'hira', 'kata', 'hankata', 'zenkaku'} do
+  for _, v in ipairs { "eiji", "hira", "kata", "hankata", "zenkaku" } do
     local mode = self[v]
     local w = fn.strdisplaywidth(mode.text)
-    if w > self.width then self.width = w end
-    local hl_name = opts[v..'_hl_name']
+    if w > self.width then
+      self.width = w
+    end
+    local hl_name = opts[v .. "_hl_name"]
     local ok, hl = pcall(api.get_hl_by_name, hl_name, true)
-    if ok then mode.hl = hl end
+    if ok then
+      mode.hl = hl
+    end
     api.set_hl(opts.ns, mode.hl_name, mode.hl)
   end
 
   return self
 end
 
-function M:is_insert() return fn.mode():find'i' end
+function M:is_insert()
+  return fn.mode():find "i"
+end
 
 function M:detect()
-  local ok, m = pcall(fn['skkeleton#mode'])
-  if not ok or m == '' then
+  local ok, m = pcall(fn["skkeleton#mode"])
+  if not ok or m == "" then
     return self.eiji
-  elseif m == 'hira' then
+  elseif m == "hira" then
     return self.hira
-  elseif m == 'kata' then
+  elseif m == "kata" then
     return self.kata
-  elseif m == 'zenkaku' then
+  elseif m == "zenkaku" then
     return self.zenkaku
   end
   return self.hankata
