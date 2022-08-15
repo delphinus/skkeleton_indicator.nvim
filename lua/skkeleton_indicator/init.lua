4ad07f4dd79233b2278cc216b19a8393eaedcbf2
delphinus <me@delphinus.dev>
2022-08-15 18:18:47 +0900

    Fix by stylua

--- lua/skkeleton_indicator/init.lua
+++ lua/skkeleton_indicator/init.lua
@@ -1,30 +1,28 @@
-local Indicator = require'skkeleton_indicator.indicator'
-local snake_case_dict = require'skkeleton_indicator.util'.snake_case_dict
+local Indicator = require("skkeleton_indicator.indicator")
+local snake_case_dict = require("skkeleton_indicator.util").snake_case_dict
 
 local M = {}
 
 function M.setup(opts)
-  M.instance = Indicator.new(
-    snake_case_dict(
-      vim.tbl_extend('keep', opts or {}, {
-        moduleName = 'skkeleton_indicator',
-        eijiHlName = 'SkkeletonIndicatorEiji',
-        hiraHlName = 'SkkeletonIndicatorHira',
-        kataHlName = 'SkkeletonIndicatorKata',
-        hankataHlName = 'SkkeletonIndicatorHankata',
-        zenkakuHlName = 'SkkeletonIndicatorZenkaku',
-        eijiText = '英字',
-        hiraText = 'ひら',
-        kataText = 'カタ',
-        hankataText = '半ｶﾀ',
-        zenkakuText = '全英',
-        alwaysShown = true,
-        fadeOutMs = 3000,
-        ignoreFt = {},
-        bufFilter = function() return true end,
-      })
-    )
-  )
+  M.instance = Indicator.new(snake_case_dict(vim.tbl_extend("keep", opts or {}, {
+    moduleName = "skkeleton_indicator",
+    eijiHlName = "SkkeletonIndicatorEiji",
+    hiraHlName = "SkkeletonIndicatorHira",
+    kataHlName = "SkkeletonIndicatorKata",
+    hankataHlName = "SkkeletonIndicatorHankata",
+    zenkakuHlName = "SkkeletonIndicatorZenkaku",
+    eijiText = "英字",
+    hiraText = "ひら",
+    kataText = "カタ",
+    hankataText = "半ｶﾀ",
+    zenkakuText = "全英",
+    alwaysShown = true,
+    fadeOutMs = 3000,
+    ignoreFt = {},
+    bufFilter = function()
+      return true
+    end,
+  })))
 end
 
 return M
--- lua/skkeleton_indicator/util.lua
+++ lua/skkeleton_indicator/util.lua
@@ -5,7 +5,7 @@ local M = {
   }, {
     __index = function(self, name)
       if not self._cache[name] then
-        self._cache[name] = vim.api['nvim_'..name]
+        self._cache[name] = vim.api["nvim_" .. name]
       end
       return self._cache[name]
     end,
@@ -13,14 +13,16 @@ local M = {
 }
 
 function M.snake_case(str)
-  return str:gsub('[A-Z]?[a-z]+', function(part)
-    return part:match'^[A-Z]' and '_'..part:lower() or part:lower()
+  return str:gsub("[A-Z]?[a-z]+", function(part)
+    return part:match("^[A-Z]") and "_" .. part:lower() or part:lower()
   end)
 end
 
 function M.snake_case_dict(opts)
   local dict = {}
-  for k, v in pairs(opts) do dict[M.snake_case(k)] = v end
+  for k, v in pairs(opts) do
+    dict[M.snake_case(k)] = v
+  end
   return dict
 end
 
